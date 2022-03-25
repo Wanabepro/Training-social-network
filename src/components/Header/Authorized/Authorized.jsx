fe4dd8a3b859b27aad19b6bbe09a88d4e730fdbd
Wanabepro <ilyasov_sk@mail.ru>
2022-03-26 00:17:15 +0300

    Fixes for gh pages

--- src/components/Header/Authorized/Authorized.jsx
+++ src/components/Header/Authorized/Authorized.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import authorizedStyles from './Authorized.module.css'
+import avatar from './../../../Icons/User-avatar.png'
 
 const Authorized = (props) => {
     return (
         <div className={authorizedStyles.auth}>
             <div className={authorizedStyles.avatar}>
-                <img src={props.photo ? props.photo : '/User-avatar.png'} alt="avatar" />
+                <img src={props.photo ? props.photo : avatar} alt="avatar" />
             </div>
             <div>
                 <p>{props.login}</p>
